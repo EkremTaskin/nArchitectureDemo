434955c2934ff3b1e6310c3d5d8ece6e749c2e02
ekrem <ekrem@DESKTOP-VUKKAQM>
2023-08-31 22:28:46 -0500

    Add project files.

---
+++ src/rentACar/RentACar/WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Application.Features.Brands.Commands.Create;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : BaseController
+    {
+        [HttpPost]
+        public  async Task<IActionResult> Add([FromBody]CreateBrandCommand createBrandCommand)
+        {
+            CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+            return Ok(response);
+        }
+    }
+}
