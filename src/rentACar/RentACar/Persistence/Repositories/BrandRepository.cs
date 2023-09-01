8b8d57ffa9fde28a3a543317d548dc6ab7901ce6
Ekrem Taskin <ekremtaskin1@hotmail.com>
2023-09-01 15:49:34 -0500

    9/1/2023

---
+++ src/rentACar/RentACar/Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistence.Repositories;
+using Persistence.Context;
+using Application.Services.Repositories;
+
+namespace Persistence.Repositories;
+
+public class BrandRepository : EfRepositoryBase<Brand, Guid, BaseDbContext>, IBrandRepository
+{
+    public BrandRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
