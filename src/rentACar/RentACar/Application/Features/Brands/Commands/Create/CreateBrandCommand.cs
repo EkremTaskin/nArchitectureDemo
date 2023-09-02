23e1950ba9c0befa84272441fae203936abe519d
Ekrem <eko20042004@hotmail.com>
2023-09-02 14:22:01 -0500

    9/2/2023

--- src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ src/rentACar/RentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,4 +1,7 @@
-﻿using MediatR;
+﻿using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,17 +12,29 @@ namespace Application.Features.Brands.Commands.Create;
 
 public class CreateBrandCommand : IRequest<CreatedBrandResponse>
 {
-    public string Name { get; set; }
+	public string Name { get; set; }
 
-    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
-    {
-        public Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
-        {
-            CreatedBrandResponse createdBrandResponse = new CreatedBrandResponse();
-            createdBrandResponse.Name = request.Name;
-            createdBrandResponse.Id = new Guid();
-            return null;
-        }
-    }
-    //
+	public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+	{
+		private readonly IBrandRepository _brandRepository;
+		private readonly IMapper _mapper;
+
+		public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+		{
+			_brandRepository = brandRepository;
+			_mapper = mapper;
+		}
+
+		public async Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+		{
+			Brand brand = _mapper.Map<Brand>(request);
+			brand.Id = Guid.NewGuid();
+
+			await _brandRepository.AddAsync(brand);
+
+			CreatedBrandResponse createdBrandResponse = _mapper.Map<CreatedBrandResponse>(brand);
+			return createdBrandResponse;
+
+		}
+	}
 }
